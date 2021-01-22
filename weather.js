ac86729b90f250cec89b3bb6c7d4f86f44cea408
ChaDongHyun <b07882002@gmail.com>
2021-01-22 10:40:13 +0900

    Add Weather Information(weather.js)

---
+++ weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "6b7a20b4370a1243ebe7125e27078b09";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+  //then()은 데이터가 다 들어온 후에 함수를 실행한다.
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+function handleGeoSucess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    // latitude: latitude,
+    // longitude: longitude,
+    latitude,
+    longitude, // Javascript에서 객체에 변수의 이름과 객체의 key 이름이 같을 경우 한번만 입력하면 됨
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("can't access geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+function init() {
+  loadCoords();
+}
+
+init();
