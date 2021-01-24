a8cf2fbf402b5eeb4ff38cc046bfe6ad99da1fb6
ChaDongHyun <b07882002@gmail.com>
2021-01-25 00:08:44 +0900

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,48 @@
+const footer = body.querySelector("footer");
+
+function paintIcon() {
+  const gitLink = document.createElement("a");
+  const blogLink = document.createElement("a");
+  const proLink = document.createElement("a");
+  const baekLink = document.createElement("a");
+  const nomadLink = document.createElement("a");
+
+  gitLink.href = `https://github.com`;
+  blogLink.href = `https://piemonte93.github.io`;
+  proLink.href = `https://programmers.co.kr/`;
+  baekLink.href = `https://www.acmicpc.net/`;
+  nomadLink.href = `https://nomadcoders.co/`;
+
+  const github = document.createElement("img");
+  const blog = document.createElement("img");
+  const programmers = document.createElement("img");
+  const baekjoon = document.createElement("img");
+  const nomad = document.createElement("img");
+  github.src = `https://github.com/fluidicon.png`;
+  blog.src = `https://section.blog.naver.com/favicon.ico?3`;
+  programmers.src = `https://programmers.co.kr/assets/icons/favicon-40b78633b6556a68c3da8e2125c31512fbd01d09906ab76c8a8ff289e494cadb.png`;
+  baekjoon.src = `https://www.acmicpc.net/apple-touch-icon.png`;
+  nomad.src = `https://nomadcoders.co/m.png`;
+  github.classList.add("icon");
+  blog.classList.add("icon");
+  programmers.classList.add("icon");
+  baekjoon.classList.add("icon");
+  nomad.classList.add("icon");
+
+  gitLink.appendChild(github);
+  blogLink.appendChild(blog);
+  proLink.appendChild(programmers);
+  baekLink.appendChild(baekjoon);
+  nomadLink.appendChild(nomad);
+
+  footer.appendChild(gitLink);
+  footer.appendChild(blogLink);
+  footer.appendChild(proLink);
+  footer.appendChild(baekLink);
+  footer.appendChild(nomadLink);
+}
+function init() {
+  paintIcon();
+}
+
+init();
