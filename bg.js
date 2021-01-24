73a8ace2139864f17138c2c41032e164cf16dd9d
ChaDongHyun <b07882002@gmail.com>
2021-01-25 02:16:27 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,18 +1,18 @@
 const body = document.querySelector("body");
-
 const IMG_NUMBER = 9; // 사진 갯수
+const image = new Image();
 
 function handleImgLoad() {
+  console.log("loading");
   body.prepend(image);
 }
 
 function paintImage(imgNumber) {
-  const image = new Image();
   image.src = `https://github.com/piemonte93/vanilla-javascript-app/blob/master/images/${
     imgNumber + 1
   }.JPG?raw=true`;
   image.classList.add("bgImage");
-  image.addEventListener("loadend", handleImgLoad);
+  image.addEventListener("load", handleImgLoad);
 }
 
 function genRandom() {
