aa7d0bef7ea164194e4223888ebbe0f57d29e187
ChaDongHyun <b07882002@gmail.com>
2021-01-25 00:40:43 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,9 +8,7 @@ const IMG_NUMBER = 9; // 사진 갯수
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `https://piemonte93.github.io/vanilla-javascript-app/${
-    imgNumber + 1
-  }.JPG`;
+  image.src = `piemonte93/vanilla-javascript-app/${imgNumber + 1}.JPG`;
   image.classList.add("bgImage");
   body.prepend(image);
   //   image.addEventListener("loadend", handleImgLoad);
