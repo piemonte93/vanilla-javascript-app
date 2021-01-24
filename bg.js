02aa312533d8a97d0a70ad1695c3134e77ca80f0
ChaDongHyun <b07882002@gmail.com>
2021-01-25 00:33:02 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ const IMG_NUMBER = 9; // 사진 갯수
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `./${imgNumber + 1}.jpg`;
+  image.src = `/repo/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
   //   image.addEventListener("loadend", handleImgLoad);
